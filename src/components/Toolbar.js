84cb355091e678ec631e6c3362e73a61eadd40af
Abhishek Singh <abhishek@webiixx.com>
2020-01-16 12:24:43 +0530

    migrate to functional

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,112 +1,67 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {View, StyleSheet, StatusBar} from 'react-native';
 import Text from './Text';
 import Button from './Button';
 import Icon from './IconNB';
-import {withNavigation} from 'react-navigation';
-import {connect} from 'react-redux';
+import {useSelector} from 'react-redux';
+import {useNavigation, useNavigationState} from 'react-navigation-hooks';
 
-class Toolbar extends Component {
-  _goTo = route => () => {
-    this.props.navigation.navigate(route);
+function Toolbar({title, menuButton, backButton, wishListButton, cartButton}) {
+  const navigation = useNavigation();
+  const {routeName} = useNavigationState();
+  const appSettings = useSelector(state => state.appSettings);
+  const count = useSelector(state => state.cartCount);
+
+  const goTo = route => () => {
+    navigation.navigate(route);
   };
 
-  _goBack = () => {
-    this.props.navigation.goBack(null);
+  const goBack = () => {
+    navigation.goBack(null);
   };
 
-  render() {
-    const {
-      appSettings,
-      menuButton,
-      backButton,
-      wishListButton,
-      cartButton,
-      count,
-      navigation: {state},
-    } = this.props;
-    return (
-      <>
-        <StatusBar
-          backgroundColor={appSettings.primary_color_dark}
-          barStyle="light-content"
-        />
-        <View
-          style={[
-            styles.container,
-            {backgroundColor: appSettings.primary_color},
-          ]}>
-          {menuButton && (
-            <Button
-              onPress={this.props.navigation.openDrawer}
-              style={styles.menuButton}>
-              <Icon
-                color={appSettings.primary_color_text}
-                name="md-menu"
-                size={24}
-              />
-            </Button>
-          )}
-          {backButton && (
-            <Button onPress={this._goBack} style={styles.menuButton}>
-              <Icon
-                color={appSettings.primary_color_text}
-                name="md-arrow-back"
-                size={24}
-              />
-            </Button>
-          )}
-          <Text style={[styles.title, {color: appSettings.primary_color_text}]}>
-            {this.props.title || state.routeName}
-          </Text>
+  return (
+    <>
+      <StatusBar backgroundColor={appSettings.primary_color_dark} barStyle="light-content" />
+      <View style={[styles.container, {backgroundColor: appSettings.primary_color}]}>
+        {menuButton && (
+          <Button onPress={navigation.openDrawer} style={styles.menuButton}>
+            <Icon color={appSettings.primary_color_text} name="md-menu" size={24} />
+          </Button>
+        )}
+        {backButton && (
+          <Button onPress={goBack} style={styles.menuButton}>
+            <Icon color={appSettings.primary_color_text} name="md-arrow-back" size={24} />
+          </Button>
+        )}
 
-          <View style={styles.right}>
-            {wishListButton && (
-              <Button
-                onPress={this._goTo('WishlistScreen')}
-                style={styles.menuButton}>
-                <Icon
-                  color={appSettings.primary_color_text}
-                  name="md-heart"
-                  size={24}
-                />
-              </Button>
-            )}
-            {cartButton && (
-              <Button onPress={this._goTo('Cart')} style={styles.menuButton}>
-                <Icon
-                  color={appSettings.primary_color_text}
-                  name="md-cart"
-                  size={24}
-                />
-                <Text
-                  style={[
-                    styles.badge,
-                    {
-                      backgroundColor:
-                        appSettings.toolbarbadgecolor ||
-                        appSettings.accent_color,
-                    },
-                  ]}>
-                  <Text style={{color: 'white', fontWeight: '700'}}>
-                    {count}
-                  </Text>
-                </Text>
-              </Button>
-            )}
-          </View>
+        <Text style={[styles.title, {color: appSettings.primary_color_text}]}>
+          {title || routeName}
+        </Text>
+
+        <View style={styles.right}>
+          {wishListButton && (
+            <Button onPress={goTo('WishlistScreen')} style={styles.menuButton}>
+              <Icon color={appSettings.primary_color_text} name="md-heart" size={24} />
+            </Button>
+          )}
+          {cartButton && (
+            <Button onPress={goTo('Cart')} style={styles.menuButton}>
+              <Icon color={appSettings.primary_color_text} name="md-cart" size={24} />
+              <Text
+                style={[
+                  styles.badge,
+                  {backgroundColor: appSettings.toolbarbadgecolor || appSettings.accent_color},
+                ]}>
+                {count}0
+              </Text>
+            </Button>
+          )}
         </View>
-      </>
-    );
-  }
+      </View>
+    </>
+  );
 }
-
-const mapStateToProps = state => {
-  return {
-    appSettings: state.appSettings,
-    count: state.cartCount,
-  };
-};
 
 const styles = StyleSheet.create({
   container: {
@@ -117,15 +72,17 @@ const styles = StyleSheet.create({
   },
   badge: {
     position: 'absolute',
-    end: -4,
-    top: -4,
-    transform: [{scale: 0.7}],
-    width: 25,
-    height: 25,
-    borderRadius: 15,
+    end: 4,
+    top: 4,
+    //transform: [{scale: 0.7}],
+    width: 20,
+    height: 20,
+    borderRadius: 10,
     fontSize: 10,
     textAlign: 'center',
     textAlignVertical: 'center',
+    color: 'white',
+    fontWeight: '600',
   },
   title: {
     fontWeight: '600',
@@ -139,4 +96,4 @@ const styles = StyleSheet.create({
   menuButton: {padding: 16},
 });
 
-export default connect(mapStateToProps)(withNavigation(Toolbar));
+export default Toolbar;
