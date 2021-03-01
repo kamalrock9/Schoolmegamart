fb544305fd33d6aa04f7b124b7cdeb8df4570943
Kamlesh Gangwar <kamlesh@webiixx.com>
2021-03-01 16:03:25 +0530

    No change

--- src/service/ApiClient.js
+++ src/service/ApiClient.js
@@ -9,28 +9,6 @@ class Service {
     let service = axios.create({
       baseURL: Constants.baseURL + Constants.path,
     });
-    // axiosRetry(service, {retries: 3, retryDelay: axiosRetry.exponentialDelay});
-
-    // service.interceptors.request.use(
-    //   config => {
-    //     config.metadata = {startTime: new Date()};
-    //     return config;
-    //   },
-    //   error => Promise.reject(error),
-    // );
-
-    // service.interceptors.response.use(
-    //   response => {
-    //     response.config.metadata.endTime = new Date();
-    //     response.duration = response.config.metadata.endTime - response.config.metadata.startTime;
-    //     return response;
-    //   },
-    //   error => {
-    //     error.config.metadata.endTime = new Date();
-    //     error.duration = error.config.metadata.endTime - error.config.metadata.startTime;
-    //     return Promise.reject(error);
-    //   },
-    // );
 
     service.interceptors.response.use(this.handleSuccess, this.handleError);
     this.service = service;
@@ -62,24 +40,6 @@ class Service {
     return this.service.get(path, {params: params});
   }
 
-  // async get(path, params = {}) {
-  //   const metric = await perf().newHttpMetric(path, "GET");
-  //   await metric.start();
-  //   return this.service
-  //     .get(path, {params: params})
-  //     .then(async res => {
-  //       metric.putAttribute("RESPONSE_TIME", res.duration.toString());
-  //       metric.setHttpResponseCode(res.status);
-  //       await metric.stop();
-  //       return res;
-  //     })
-  //     .catch(async err => {
-  //       metric.setHttpResponseCode(0);
-  //       await metric.stop();
-  //       return err;
-  //     });
-  // }
-
   patch(path, payload) {
     return this.service.request({
       method: "PATCH",
@@ -98,30 +58,6 @@ class Service {
       ...extra,
     });
   }
-
-  // async post(path, payload, extra = {}) {
-  //   const metric = await perf().newHttpMetric(path, "POST");
-  //   await metric.start();
-  //   return this.service
-  //     .request({
-  //       method: "POST",
-  //       url: path,
-  //       responseType: "json",
-  //       data: payload,
-  //       ...extra,
-  //     })
-  //     .then(async res => {
-  //       metric.putAttribute("RESPONSE_TIME", res.duration.toString());
-  //       metric.setHttpResponseCode(res.status);
-  //       await metric.stop();
-  //       return res;
-  //     })
-  //     .catch(async err => {
-  //       metric.setHttpResponseCode(0);
-  //       await metric.stop();
-  //       return err;
-  //     });
-  // }
 
   put(path, payload, extra = {}) {
     return this.service.request({
